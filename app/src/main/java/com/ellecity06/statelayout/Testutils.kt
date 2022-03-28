37e71b51ffb6c6f14ccdf21b9ac8225665b8c401
zhouziwei <zhouziwei@gainhon.com>
2022-03-28 15:18:29 +0800

    edit gitignore

---
+++ app/src/main/java/com/ellecity06/statelayout/Testutils.kt
@@ -0,0 +1,16 @@
+package com.ellecity06.statelayout
+
+import android.util.Log
+
+/**
+ * @author ellecity06
+ * @time 2022/3/28 15:16
+ * @des
+ */
+class Testutils {
+    companion object{
+        fun testMessage(){
+            Log.d("zavier","测试组件")
+        }
+    }
+}
